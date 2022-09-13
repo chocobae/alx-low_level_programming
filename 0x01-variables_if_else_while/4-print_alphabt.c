7f696a5218a906056e0eccef0d1b1c9406378fb5
Ann <chocobae1995@gmail.com>
2022-09-12 18:42:47 -0700

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,16 @@
 * Return: Always 0 (Success)
 */
 
-int main(void){
-int i;
+int main(void)
 {
+int i;
 for (i = 97; i < 123; i++)
+{
 if (i != 101 && i != 113)
-}	
-{				
+{
 putchar(i);
+}
+}
 putchar('\n');
 return (0);
 }
