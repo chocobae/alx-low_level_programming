8071dc2c64a0b268845550197f2369b8f7cae966
chocobae <Chocobae1995@gmail.com>
2022-09-21 17:31:06 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -12,29 +12,29 @@ int main(void)
 	char *ptr;
 	int i;
 
-	        for (i = 0; i < 98 - 1; i++)
+	for (i = 0; i < 98 - 1; i++)
+	{
+		s1[i] = '*';
+	}
+	s1[i] = '\0';
+	printf("%s\n", s1);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+	printf("%s\n", s1);
+	printf("%s\n", ptr);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+	printf("%s", s1);
+	printf("%s", ptr);
+	for (i = 0; i < 98; i++)
+	{
+		if (i % 10)
 		{
-			s1[i] = '*';
+		printf(" ");
 		}
-		s1[i] = '\0';
-		printf("%s\n", s1);
-		ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-		printf("%s\n", s1);
-		printf("%s\n", ptr);
-		ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-		printf("%s", s1);
-		printf("%s", ptr);
-		for (i = 0; i < 98; i++)								        {
-
-			if (i % 10)
-			{
-				printf(" ");
-			}
-			if (!(i % 10) && i)
-			{
-				printf("\n");
-			}
-			printf("0x%02x", s1[i]);
+		if (!(i % 10) && i)
+		}
+		printf("\n");
+		}
+		printf("0x%02x", s1[i]);
 		}
 		printf("\n");
 		return (0);
