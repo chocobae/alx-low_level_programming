275d8fd830b95552819ed17b48ca2dc7d5e3c466
chocobae <Chocobae1995@gmail.com>
2022-10-07 05:43:52 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,55 +3,55 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * * _isNum - check if is a number
- * *@num: string to check
- * *Return: 1 is numm, 0 not num
- * */
+* _isNum - check if is a number
+*@num: string to check
+*Return: 1 is numm, 0 not num
+*/
 int _isNum(char *num)
 {
-		int i;
+	int i;
 
-			for (i = 0; num[i] != '\0'; i++)
-					{
-								if (num[i] < '0' || num[i] > '9')
-												return (0);
-									}
-				return (1);
+	for (i = 0; num[i] != '\0'; i++)
+	{
+		if (num[i] < '0' || num[i] > '9')
+			return (0);
+	}
+	return (1);
 }
 
 /**
- * * *_memset - copies a character to the firstn characters of the string pointed
- * *@s: original string
- * *@b: value to remplace
- * *@n: number of bytes
- * *Return: s (string modify)
- * */
+**_memset - copies a character to the firstn characters of the string pointed
+*@s: original string
+*@b: value to remplace
+*@n: number of bytes
+*Return: s (string modify)
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-		unsigned int i;
+	unsigned int i;
 
-			for (i = 0; i < n; i++)
-						s[i] = b;
-				return (s);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
 
 /**
- * * _strlen - returns the lenght of a string
- * *@s: poiter of character
- * *Return: the length of a string
- * */
+* _strlen - returns the lenght of a string
+*@s: poiter of character
+*Return: the length of a string
+*/
 int _strlen(char *s)
 {
-		int len;
+	int len;
 
-			len = 0;
-				while (*(s + len) != '\0')
-							len++;
-					return (len);
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
 }
 
 /**
-* main - multiple 2 positive numbers
+*main - multiple 2 positive numbers
 *@argc: argument counter
 *@argv: number to multiply
 *Return: 0 (success)
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 	int *res;
 
 	if ((argc != 3 || !(_isNum(argv[1]))) || !(_isNum(argv[2])))
-		puts("Error"), exit(98);
+	puts("Error"), exit(98);
 	l1 = _strlen(argv[1]), l2 = _strlen(argv[2]);
 	length = l1 + l2;
 	res = calloc(length, sizeof(int *));
@@ -86,7 +86,7 @@ int main(int argc, char *argv[])
 	}
 
 	if (res[0] == 0)
-		i = 1;
+	i = 1;
 	else
 		i = 0;
 	for (; i < length; i++)
@@ -95,4 +95,3 @@ int main(int argc, char *argv[])
 	free(res);
 	return (0);
 }
-
