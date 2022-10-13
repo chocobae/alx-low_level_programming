9ba939baaebb0b194170fd0ee425490f57e6ee9b
chocobae <Chocobae1995@gmail.com>
2022-10-12 20:48:35 -0700

    task1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,12 +1,11 @@
 #include "function_pointers.h"
 
 /*
-* array_iterator - executes a function given as a parameter
-* on each element of an array
-*
-* @array: pointer to array
+* array_iterator - executes a function given as a parameter on each element of an array
+* @arrayi: pointer to array
 * @size: size of array
 * @action: function to call for each element of array
+*
 * Return: nothing
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
@@ -18,7 +17,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
-
 		}
 	}
 }
