ef72ba2789549f82dde44d2a35b453a1fc081df8
chocobae <Chocobae1995@gmail.com>
2022-10-12 20:32:09 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,6 +14,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	if (array && size > 0 && action)
+	{
 		for (i = 0; i < size; i++)
+		{
 			action(array[i]);
+
+		}
+	}
 }
